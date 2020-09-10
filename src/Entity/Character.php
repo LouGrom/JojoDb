a0f83261fd692347dbdce60da44b14ecd7535767
LouGrom <lou.gromenil@gmail.com>
2020-09-10 13:50:28 +0200

    Add validators entities

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -6,6 +6,7 @@ use App\Repository\CharacterRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CharacterRepository::class)
@@ -22,6 +23,8 @@ class Character
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(max=255)
      */
     private $name;
 

9a466b25798bef48c3fb4a1f99aacddb5ebf09b4
LouGrom <lou.gromenil@gmail.com>
2020-09-10 12:55:41 +0200

    Add CRUD season

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label'=>'Nom de la saison'])
+            ->add('date', DateType::class, ['label'=>'Date de diffusion', 'widget'=>'single_text'])
+            ->add('picture', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4096k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/gif',
+                            'image/png',
+                            'image/bmp'
+                        ],
+                        'mimeTypesMessage' => "Le format n'est pas accepté, veuillez choisir un fichier au format .jpg, .png ou .gif"
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class, ['label'=>'Description'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}

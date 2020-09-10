74cdceb9d8175a11d3c021accd8fede2dc9abbf9
LouGrom <lou.gromenil@gmail.com>
2020-09-10 11:33:34 +0200

    Add characters list

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -8,6 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CharacterController extends AbstractController
 {
+   /**
+     * @Route("/characters", name="characters_list")
+     */
+    public function list(CharacterRepository $characterRepository)
+    {
+        return $this->render('character/list.html.twig', ['characters'=>$characterRepository->findAll()]);
+    }
+   
     /**
      * @Route("/character/{id}/view", name="character_view", requirements={"id" = "\d+"})
      */

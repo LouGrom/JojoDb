4bb2506d34c4cec67590057b22a3d040653e594f
LouGrom <lou.gromenil@gmail.com>
2020-09-10 13:43:52 +0200

    Add CRUD stands

--- src/Controller/StandController.php
+++ src/Controller/StandController.php
@@ -2,17 +2,99 @@
 
 namespace App\Controller;
 
+use App\Entity\Stand;
+use App\Form\StandType;
 use App\Repository\StandRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/stand")
+ */
 class StandController extends AbstractController
 {
     /**
-     * @Route("/stands", name="stands_list")
+     * @Route("/", name="stand_list", methods={"GET"})
      */
-    public function list(StandRepository $standRepository)
+    public function list(StandRepository $standRepository): Response
     {
-        return $this->render('stand/list.html.twig', ['stands'=>$standRepository->findAll()]);
+        return $this->render('stand/list.html.twig', [
+            'stands' => $standRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="stand_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $stand = new Stand();
+        $form = $this->createForm(StandType::class, $stand);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($stand);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Le stand a bien été ajouté");
+
+            return $this->redirectToRoute('stand_list');
+        }
+
+        return $this->render('stand/new.html.twig', [
+            'stand' => $stand,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="stand_show", methods={"GET"})
+     */
+    public function show(Stand $stand): Response
+    {
+        return $this->render('stand/show.html.twig', [
+            'stand' => $stand,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="stand_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Stand $stand): Response
+    {
+        $form = $this->createForm(StandType::class, $stand);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash("success", "Le stand a bien été modifié");
+
+            return $this->redirectToRoute('stand_list');
+        }
+
+        return $this->render('stand/edit.html.twig', [
+            'stand' => $stand,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="stand_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Stand $stand): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$stand->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($stand);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Le stand a bien été supprimé");
+        }
+
+        return $this->redirectToRoute('stand_list');
     }
 }
---
+++ src/Form/StandType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stand;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class StandType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label'=>'Nom du stand'])
+            ->add('description', TextareaType::class, ['label'=>'Description'])
+            ->add('picture', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4096k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/gif',
+                            'image/png',
+                            'image/bmp'
+                        ],
+                        'mimeTypesMessage' => "Le format n'est pas accepté, veuillez choisir un fichier au format .jpg, .png ou .gif"
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Stand::class,
+        ]);
+    }
+}

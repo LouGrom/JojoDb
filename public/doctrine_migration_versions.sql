93a321cd7d63aae13ec4a97c40eb63a7451cb01e
LouGrom <lou.gromenil@gmail.com>
2020-09-11 15:45:22 +0200

    front characters and stands list

--- public/doctrine_migration_versions.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20200908121035',	'2020-09-08 17:00:19',	40),
-('DoctrineMigrations\\Version20200908122207',	'2020-09-08 17:00:19',	63),
-('DoctrineMigrations\\Version20200908122422',	'2020-09-08 17:00:20',	80),
-('DoctrineMigrations\\Version20200908123138',	'2020-09-08 17:00:20',	89),
-('DoctrineMigrations\\Version20200908150917',	'2020-09-08 17:09:24',	103),
-('DoctrineMigrations\\Version20200908151733',	'2020-09-08 17:17:38',	41);
